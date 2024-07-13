a9e08aa1273e9a6b8c8ced4c62a3aa3b1ba9208b
kelvincarvalho <kelvin.nirvana@gmail.com>
2024-07-13 17:05:06 -0300

    feat: refactoring destination and confirm trip

---
+++ src/pages/create-trip/index.tsx
@@ -0,0 +1,131 @@
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { InviteGuestsModal } from "./invite-guests-modal";
+import { ConfirmTripModal } from "../trip-details/confirm-trip-modal";
+import { DestinationAndDateStep } from "./steps/destination-and-date-step";
+import { InviteGuestsStep } from "./steps/invite-guests-step";
+
+export function CreateTripPage() {
+  const navigate = useNavigate();
+
+  const [isGuestsIpuntOpen, setIsGuestsIpuntOpen] = useState(false);
+  const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false);
+  const [emailsToInvite, setEmailsToInvite] = useState(["kelvin@cuid.com.br"]);
+  const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false);
+
+  function openGuestsInput() {
+    setIsGuestsIpuntOpen(true);
+  }
+
+  function closeGuestsInput() {
+    setIsGuestsIpuntOpen(false);
+  }
+
+  function openGuestsModal() {
+    setIsGuestsModalOpen(true);
+  }
+
+  function closeGuestsModal() {
+    setIsGuestsModalOpen(false);
+  }
+
+  function openConfirmTripModal() {
+    setIsConfirmTripModalOpen(true);
+  }
+
+  function closeConfirmTripModal() {
+    setIsConfirmTripModalOpen(false);
+  }
+
+  function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+    const email = data.get("email")?.toString();
+
+    if (!email) {
+      return;
+    }
+
+    if (emailsToInvite.includes(email)) {
+      alert("E-mail já cadastrado!");
+      return;
+    }
+
+    setEmailsToInvite([...emailsToInvite, email]);
+
+    event.currentTarget.reset();
+  }
+
+  function removeEmailFromInvites(emailToRemove: string) {
+    const nemEmailList = emailsToInvite.filter(
+      (email) => email !== emailToRemove,
+    );
+
+    setEmailsToInvite(nemEmailList);
+  }
+
+  function createTrip(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    navigate("trips/123");
+  }
+
+  return (
+    <div className="flex h-screen items-center justify-center bg-pattern bg-center bg-no-repeat">
+      <div className="w-full max-w-3xl space-y-10 px-6 text-center">
+        <div className="flex flex-col items-center gap-3">
+          <img src="/logo.svg" alt="plann.er logo" />
+          <p className="text-lg text-zinc-300">
+            Convide seus amigos e planeje sua próxima viagem!
+          </p>
+        </div>
+
+        <div className="space-y-4">
+          <DestinationAndDateStep
+            closeGuestsInput={closeGuestsInput}
+            isGuestsIpuntOpen={isGuestsIpuntOpen}
+            openGuestsInput={openGuestsInput}
+          />
+
+          {isGuestsIpuntOpen && (
+            <InviteGuestsStep
+              openConfirmTripModal={openConfirmTripModal}
+              emailsToInvite={emailsToInvite}
+              openGuestsModal={openGuestsModal}
+            />
+          )}
+        </div>
+
+        <p className="text-sm text-zinc-500">
+          Ao planejar sua viagem pelo plann.er você automaticamente concorda
+          <br /> com nossos{" "}
+          <a className="text-zinc-300" href="">
+            termos de uso
+          </a>{" "}
+          e{" "}
+          <a className="text-zinc-300" href="">
+            políticas de privacidade
+          </a>
+          .
+        </p>
+      </div>
+
+      {isGuestsModalOpen && (
+        <InviteGuestsModal
+          emailsToInvite={emailsToInvite}
+          closeGuestsModal={closeGuestsModal}
+          addNewEmailToInvite={addNewEmailToInvite}
+          removeEmailFromInvites={removeEmailFromInvites}
+        />
+      )}
+
+      {isConfirmTripModalOpen && (
+        <ConfirmTripModal
+          closeConfirmTripModal={closeConfirmTripModal}
+          createTrip={createTrip}
+        />
+      )}
+    </div>
+  );
+}
---
+++ src/pages/create-trip/steps/invite-guests-step.tsx
@@ -0,0 +1,44 @@
+import { ArrowRight, UserRoundPlus } from "lucide-react";
+
+interface InviteGuestsStepProps {
+  openGuestsModal: () => void;
+  openConfirmTripModal: () => void;
+  emailsToInvite: string[];
+}
+
+export function InviteGuestsStep({
+  openConfirmTripModal,
+  openGuestsModal,
+  emailsToInvite,
+}: InviteGuestsStepProps) {
+  return (
+    <div className="flex h-16 items-center gap-3 rounded-xl bg-zinc-900 px-4 shadow-shape">
+      <button
+        type="button"
+        onClick={openGuestsModal}
+        className="flex flex-1 items-center gap-2"
+      >
+        <UserRoundPlus className="size-5 text-zinc-400" />
+        {emailsToInvite.length > 0 ? (
+          <span className="flex-1 text-left text-lg text-zinc-100">
+            {emailsToInvite.length} pessoa(s) convidada(s)
+          </span>
+        ) : (
+          <span className="flex-1 text-left text-lg text-zinc-400">
+            Quem estará na viagem?
+          </span>
+        )}
+      </button>
+
+      <div className="h-6 w-px bg-zinc-800"></div>
+
+      <button
+        onClick={openConfirmTripModal}
+        className="flex items-center gap-2 rounded-lg bg-lime-300 px-5 py-2 font-medium text-lime-950 hover:bg-lime-400"
+      >
+        Confirmar viagem
+        <ArrowRight className="size-5" />
+      </button>
+    </div>
+  );
+}
